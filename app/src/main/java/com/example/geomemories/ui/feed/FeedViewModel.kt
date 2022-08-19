3ec0ce7b7a10506369ef34079e195fa09af17a0b
Muzahidul Islam <muzahidulislamis237@gmail.com>
2022-08-18 23:32:12 -0400

    Changed screen names

--- app/src/main/java/com/example/geomemories/ui/feed/FeedViewModel.kt
+++ app/src/main/java/com/example/geomemories/ui/feed/FeedViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.geomemories.ui.home
+package com.example.geomemories.ui.feed
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class FeedViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is feed Fragment"
     }
     val text: LiveData<String> = _text
 }
